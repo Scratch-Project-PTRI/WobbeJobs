d5bb95b4c4129033bb850ece2c09c1cc8854e5dd
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-04-11 17:20:02 -0400

    search page skeleton

---
+++ client/pages/profile-page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function EditProfile() {
+    return (
+        <div className="login-page">
+             profile editor
+        </div>
+    );
+}
+
+export default EditProfile;
