6be243117aa067cb203da3e0a7e285226c95b21d
Jewel Clarke <133067242+JewelClarkeNYC@users.noreply.github.com>
2024-04-11 22:31:04 -0400

    created schemas for auth profile and searchHistory

---
+++ models/ProfileModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profile = new Schema ({
+  firstName : {
+    type : String,
+    required : true
+  },
+  lastName : {
+    type : String,
+    required : true
+  },
+  email : {
+    type : String,
+    required : true
+  },
+  location : {
+    type : String,
+    required : true
+  },
+  resume : {
+    type : Object,
+    required : false
+  },
+})
+
+module.exports = mongoose.model ('ProfileModel', profile);
+/models
