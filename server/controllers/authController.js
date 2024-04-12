9e2cebd2fdf449ba647e9ad1a0584b3403933fb9
Wilson Chen <wilsonchen@Wilsons-MacBook-Air.local>
2024-04-12 19:48:10 -0400

    auth middleware completed with bcrypt. minor changes in other files to integrate auth

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,4 @@
-const Auth = require('../models/authModel');
+const Auth = require('../../models/AuthModel');
 const bcrypt = require('bcryptjs');
 const authController = {};
 
@@ -6,14 +6,15 @@ const authController = {};
  * createUser - create and save a nAuth into the database.
  */
 authController.createUser = (req, res, next) => {
-  const { username, password } = req.body;
-  if (username && password) {
-    console.log('Username and password provided');
+  const { email, password } = req.body;
+  if (email && password) {
+    console.log('Email and password provided');
     Auth.create({
-      username,
+      email,
       password,
     }).then((data) => {
       console.log(data);
+      res.locals.user = data;
       return next();
     }).catch(err => {
       return next({
@@ -23,32 +24,38 @@ authController.createUser = (req, res, next) => {
       });
     });
   } else {
-    return next({ error: 'Username or password was not provided' });
+    return next({ error: 'Email or password was not provided' });
   }
 };
 
 /**
- * verifyUser - Obtain username and password from the request body, locate
+ * verifyUser - Obtain email and password from the request body, locate
  * the appropriate user in the database, and then authenticate the submitted password
  * against the password stored in the database.
  */
 authController.verifyUser = (req, res, next) => {
-  const { username, password } = req.body;
-  Auth.find({ username })
+  const { email, password } = req.body;
+  Auth.find({ email })
     .then((data) => {
-      if(!data[0]) next({ error: 'Username was not found'});
-      // console.log("Find password: ", data[0].password, "username: ", username);
+      console.log(data);
+      if(!data[0]) {
+        next({ error: 'Email was not found'});
+      }
+      // console.log("Find password: ", data[0].password, "email: ", email);
       const dbPassword = data[0].password;
+      console.log(dbPassword);
       res.locals.dbId = data[0]['_id'].toString();
       console.log('dbId: ', res.locals.dbId);
       console.log('password: ', password);
-      const match = bcrypt.compare(password, dbPassword).then((data) => {
-        console.log('match: ', data);
+      const match = bcrypt.compare(password, dbPassword).then((didMatch) => {
+        // console.log('match: ', didMatch);
+        if (!didMatch) {
+          res.locals.incorrect = true;
+        } else {
+          res.locals.user = data;
+        }
+        return next();
       });
-      if (!match) {
-        res.locals.incorrect = true;
-      }
-      return next();
     }).catch(err => {
       return next({
         log: 'Express error handler caught error in authController.verifyUser',
