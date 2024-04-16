0e0c9bd9e7fab24850616facac37add4a8f4017d
Tommy Martinez <tommy@Tommys-MacBook-Pro.local>
2024-04-16 14:45:48 -0400

    Working puppeteer data flow

--- client/pages/login-page.jsx
+++ client/pages/login-page.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
 function Login() {
   const navigate = useNavigate();
 
@@ -18,7 +17,7 @@ function Login() {
   return (
     <div className="min-h-screen flex justify-center  items-center bg-gradient-to-br from-teal-50 via-cyan-100 to-green-200">
       <div className="max-w-md w-full bg-white rounded-xl shadow-2xl p-8">
-        <h2 className="text-3xl font-semibold text-center text-gray-800 mb-8">
+        <h2 className="text-3xl font-semibold text-center text-gray-700 mb-8">
           Welcome Back!
         </h2>
         <div className="mb-4">
--- client/pages/search-page.jsx
+++ client/pages/search-page.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EditProfile from './profile-page.jsx';
 import { useNavigate } from 'react-router-dom';
 
 function Search() {
   const navigate = useNavigate();
+  const [jobTitle, setJobTitle] = useState('');
+  const [jobLocation, setLocation] = useState('');
+  const [jobRadius, setRadius] = useState('');
 
   const handleEditingProfile = (e) => {
     e.preventDefault();
@@ -15,9 +18,22 @@ function Search() {
     console.log('Scrape Data');
     const fetchData = async () => {
       try {
-        //Body ---> job title & location
-        const response = await fetch('/search');
+        if (!jobTitle || !jobLocation) {
+          console.log('Job title and Location are required');
+          return;
+        }
+
+        const response = await fetch('/search', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            jobTitle,
+            jobLocation,
+            jobRadius,
+          }),
+        });
         const data = await response.json();
+        console.log('RESPONSE from Scrape ---->', data);
       } catch (error) {
         console.log('Error scraping from Front End Fetch:', error);
       }
@@ -40,20 +56,37 @@ function Search() {
       SEARCH / HOME PAGE
       <button onClick={handleEditingProfile}> Edit Profile</button>
       <div className="mb-4">
-        <label className="block text-gray-700 ">Job Title</label>
+        <label className="block text-gray-700 ">Job Title:</label>
         <input
           type="text"
           className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-red-500 focus:border-red-500"
-          placeholder="Enter your email..."
+          placeholder="Enter your desired Job Title..."
+          value={jobTitle}
+          onChange={(e) => setJobTitle(e.target.value)}
+          required
         />
       </div>
       <div className="mb-6">
-        <label className="block text-gray-700">Password:</label>
+        <label className="block text-gray-700">Location:</label>
         <input
           type="text"
-          name="password"
+          name="location"
           className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-          placeholder="Enter your password"
+          placeholder="Enter your desired Location..."
+          value={jobLocation}
+          onChange={(e) => setLocation(e.target.value)}
+          required
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-gray-700">Radius:</label>
+        <input
+          type="text"
+          name="radius"
+          className="mt-1 block w-full border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
+          placeholder="Enter your desired Radius..."
+          value={jobRadius}
+          onChange={(e) => setRadius(e.target.value)}
         />
       </div>
       <button
