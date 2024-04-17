7c1a6e10d02577d1ce83be3902b8c30efde996f9
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-04-17 18:57:01 -0400

    profile page image renders cleaned code

--- client/pages/profile-page.jsx
+++ client/pages/profile-page.jsx
@@ -1,46 +1,43 @@
-import React from 'react';
+import React from "react";
 import { useNavigate } from "react-router-dom";
-const wobblegongImg = 'https://banner2.cleanpng.com/20180527/gyy/kisspng-tasselled-wobbegong-spotted-wobbegong-bull-shark-d-5b0a328f358497.0765976515273949592192.jpg';
+const wobblegongImg =
+  "https://banner2.cleanpng.com/20180527/gyy/kisspng-tasselled-wobbegong-spotted-wobbegong-bull-shark-d-5b0a328f358497.0765976515273949592192.jpg";
 function EditProfile() {
-    const navigate = useNavigate();
-    function updateProfile (event) {
-        console.log("in updateProfile")
-    }
+  const navigate = useNavigate();
+  function updateProfile(event) {
+    console.log("in updateProfile");
+  }
 
+  return (
+    <div>
+      <div>
+        <img src={wobblegongImg} onClick={() => navigate("/home")} />
+      </div>
+      <span className="login-container">
+        <label>First Name:</label>
+        <input type="text" id="firstName" name="firstName" />
+        <br />
 
-    return (
-        <div>
+        <label>Last Name:</label>
+        <input type="text" id="lastName" name="lastName" />
+        <br />
 
-        <div> 
-            <img src={wobblegongImg} onClick={() => navigate('/home')}/>
-        </div>
-             <span className="login-container">
-                
-                <label>First Name:</label>
-                <input type="text" id="firstName" name="firstName" />
-                <br />
+        <label>Username:</label>
+        <input type="email" id="email" name="username" />
+        <br />
 
-                <label>Last Name:</label>
-                <input type="text" id="lastName" name="lastName" />
-                <br />
-
-                <label>Username:</label>
-                <input type="email" id="email" name="username" />
-                <br />
-
-                <label className="password-login">Password:</label>
-                <input type="password" id="password" name="password" />
-                <button onClick={updateProfile}>Update Profile</button>
-            </span>
-
-        </div>
-    );
+        <label className="password-login">Password:</label>
+        <input type="password" id="password" name="password" />
+        <button onClick={updateProfile}>Update Profile</button>
+      </span>
+    </div>
+  );
 }
 
 export default EditProfile;
 
-// allow users to edit firstName, lastName, email, password 
-    // input boxess for each populated with current values
+// allow users to edit firstName, lastName, email, password
+// input boxess for each populated with current values
 // stretch: upload photo, upload resume
 // once save button clicked, message confirming change "your [password/name/etc] has been updated"
 // allow users to navigate to search page -> clocking icon on fixed nav bar header
