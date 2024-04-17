2f5b2b727c6919ed8b1fab74873298b38838021c
Wilson Chen <wilsonchen@Wilsons-MacBook-Air.local>
2024-04-16 21:53:26 -0400

    fixed image link

--- client/pages/search-page.jsx
+++ client/pages/search-page.jsx
@@ -65,7 +65,7 @@ function Search() {
   return (
     <div className="search-page">
       <img
-        src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fbanner2.cleanpng.com%2F20180527%2Fgyy%2Fkisspng-tasselled-wobbegong-spotted-wobbegong-bull-shark-d-5b0a328f358497.0765976515273949592192.jpg&tbnid=Y1vaIbZCDgsAMM&vet=12ahUKEwjLz-7J0r2FAxXeBGIAHSUjDysQMygAegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.cleanpng.com%2Fpng-tasselled-wobbegong-spotted-wobbegong-bull-shark-d-2467967%2F&docid=SRhtk-GGS1SPWM&w=900&h=720&q=tasselled%20wobbegong%20cartoon&ved=2ahUKEwjLz-7J0r2FAxXeBGIAHSUjDysQMygAegQIARBP"
+        src="https://banner2.cleanpng.com/20180527/gyy/kisspng-tasselled-wobbegong-spotted-wobbegong-bull-shark-d-5b0a328f358497.0765976515273949592192.jpg"
         onClick={() => navigate('/home')}
       />
       <div>
