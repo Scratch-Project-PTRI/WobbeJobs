7d83a982c3614bc9c114243fb4accd011e319651
Wilson Chen <wilsonchen@Wilsons-MacBook-Air.local>
2024-04-18 00:46:44 -0400

    Working login authentication and edit profile updates

--- client/pages/profile-page.jsx
+++ client/pages/profile-page.jsx
@@ -3,30 +3,92 @@ import { useNavigate } from "react-router-dom";
 const wobblegongImg =
   "https://banner2.cleanpng.com/20180527/gyy/kisspng-tasselled-wobbegong-spotted-wobbegong-bull-shark-d-5b0a328f358497.0765976515273949592192.jpg";
 
-function EditProfile() {
+function EditProfile(props) {
   const navigate = useNavigate();
-  function updateProfile(event) {
-    console.log("in updateProfile");
+
+  let newEmail;
+  let newPassword;
+
+  const updateEmail = async () => {
+    try {
+      const response = await fetch("/editemail", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: props.currentEmail,
+          newEmail,
+        }),
+      });
+      if(response.status === 200) {
+        alert('Successfully updated email')
+        props.setCurrentEmail(newEmail);
+        navigate('/home');
+      } else {
+        alert('Error updating email')
+      }
+    } catch (error) {
+      console.log("Error updating from edit profile:", error);
+    }
   }
 
+  const handleUpdateEmail = (e) => {
+    e.preventDefault();
+    newEmail = document.getElementById('email').value;
+    console.log('current email: ', props.currentEmail);
+    console.log('newEmail: ', newEmail);
+    updateEmail();
+  };
+  
+  const updatePassword = async () => {
+    try {
+      const response = await fetch("/editpassword", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email: props.currentEmail,
+          newPassword,
+        }),
+      });
+      if(response.status === 200) {
+        alert('Successfully updated password')
+        navigate('/home');
+      } else {
+        alert('Error updating password')
+      }
+    } catch (error) {
+      console.log("Error updating from edit profile:", error);
+    }
+  }
+  
+    const handleUpdatePassword = (e) => {
+      e.preventDefault();
+      newPassword = document.getElementById('password').value;
+      console.log('current email: ', props.currentEmail);
+      console.log('newPassword: ', newPassword);
+      updatePassword();
+    };
+  
   return (
     <div className="search-page min-h-screen bg-gradient-to-br from-teal-50 via-cyan-100 to-green-200">
-
-  <div>
+      <div>
         <img
           src={wobblegongImg}
           className="h-10 w-auto"
           onClick={() => navigate("/home")}
         />
       </div>
+      <div className='flex justify-center'>
+        <h1 className='text-lg font-semibold mb-4'>Hello {props.currentEmail}</h1>
+      </div>
       <span className="login-container">
         <label>Username:</label>
-        <input type="email" id="email" name="username" value="*****"/>
+        <input type="email" id="email" name="username" placeholder="Enter new Email"/>
+        <button onClick={handleUpdateEmail}>Update Email</button>
         <br />
 
         <label className="password-login">Password:</label>
-        <input type="password" id="password" name="password" value="*****"/>
-        <button onClick={updateProfile}>Update Profile</button>
+        <input type="password" id="password" name="password" placeholder="Enter new Password"/>
+        <button onClick={handleUpdatePassword}>Update Password</button>
       </span>
     </div>
   );
