09d728d542933c1456adb2455291d244f26712ac
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-04-18 08:46:55 -0400

    search page comments

--- server/api/tutorial-puppeteer.js
+++ server/api/tutorial-puppeteer.js
@@ -1,4 +1,4 @@
-// const puppeteer = require("puppeteer");
+//const puppeteer = require("puppeteer");
 
 // (async () => {
 //   const browser = await puppeteer.launch({
@@ -171,7 +171,7 @@
 //   await browser.close();
 // })();
 
-////------------>>> Winning code BELOW <<<-----------\\\\\\\\\\
+////-----------ZIP ->>> Winning code BELOW <<<-----------\\\\\\\\\\
 const puppeteer = require('puppeteer');
 
 (async () => {
@@ -182,7 +182,7 @@ const puppeteer = require('puppeteer');
 
   const page = await browser.newPage();
   await page.goto(
-    'https://www.ziprecruiter.com/jobs-search?search=carpenter&location=Clearwater%2C+FL&radius=25'
+    'https://www.ziprecruiter.com/jobs-search?search=software+engineer&location=remote'
   );
 
   const data = await page.evaluate(() => {
@@ -213,10 +213,15 @@ const puppeteer = require('puppeteer');
       const quickApplyLink = quickApplyLinkElement
         ? quickApplyLinkElement.href
         : null;
+        const companyNameElement = parentElement.querySelector('[data-testid="job-card-company"]');
+        const companyName = companyNameElement
+        ? companyNameElement.textContent.trim() 
+        : 'company name';
 
-      results.push({ jobTitle, priceTitle, quickApplyLink });
+      results.push({ jobTitle, priceTitle, quickApplyLink, companyName });
+      //console.log(results)
     });
-
+    
     return results;
   });
 
@@ -225,49 +230,50 @@ const puppeteer = require('puppeteer');
   await browser.close();
 })();
 
-// -----> Winning Code Below <----------\\
-(async () => {
-  const browser = await puppeteer.launch({
-    headless: false,
-    defaultViewport: false,
-  });
+// -----> INDEED Winning Code Below <----------\\
+// (async () => {
+//   const browser = await puppeteer.launch({
+//     headless: false,
+//     defaultViewport: false,
+//   });
 
-  const page = await browser.newPage();
-  await page.goto('https://www.indeed.com/jobs?q=software+engineer&l=remote');
+//   const page = await browser.newPage();
+//   await page.goto('https://www.indeed.com/jobs?q=software+engineer&l=remote');
 
-  const data = await page.evaluate(() => {
-    const jobElements = document.querySelectorAll('.job_seen_beacon');
-    const results = [];
+//   const data = await page.evaluate(() => {
+//     const jobElements = document.querySelectorAll('.job_seen_beacon');
+//     const results = [];
 
-    jobElements.forEach((jobElement) => {
-      const title = jobElement.querySelector('.jobTitle').textContent.trim();
-      console.log(title);
+//     jobElements.forEach((jobElement) => {
+//       const title = jobElement.querySelector('.jobTitle').textContent.trim();
+//       console.log(title);
 
-      const priceTitleElement = jobElement.querySelector(
-        '.css-1cvo3fd.eu4oa1w0'
-      );
-      const priceTitle = priceTitleElement
-        ? priceTitleElement.textContent.trim()
-        : 'Salary not found';
-      // https://www.indeed.com/rc
-      const quickApplyLinkElement = jobElement.querySelector(
-        'a.jcs-JobTitle.css-jspxzf.eu4oa1w0'
-      );
+//       const priceTitleElement = jobElement.querySelector(
+//         '.css-1cvo3fd.eu4oa1w0'
+//       );
+//       const priceTitle = priceTitleElement
+//         ? priceTitleElement.textContent.trim()
+//         : 'Salary not found';
+//       // https://www.indeed.com/rc
+//       const quickApplyLinkElement = jobElement.querySelector(
+//         'a.jcs-JobTitle.css-jspxzf.eu4oa1w0'
+//       );
 
-      const quickApplyLink = quickApplyLinkElement
-        ? quickApplyLinkElement.href
-        : 'quick apply condition';
+//       const quickApplyLink = quickApplyLinkElement
+//         ? quickApplyLinkElement.href
+//         : 'quick apply condition';
 
-      results.push({ title, priceTitle, quickApplyLink });
-    });
 
-    return results;
-  });
+//       results.push({ title, priceTitle, quickApplyLink });
+//     });
 
-  console.log('Results DATA --->', data);
+//     return results;
+//   });
 
-  await browser.close();
-})();
+//   console.log('Results DATA --->', data);
+
+//   await browser.close();
+// })();
 
 //here is the element to scrape
 //  <a id="sj_414ec77a8d791e31" data-mobtk="1hrkqa2n3ia30863" data-jk="414ec77a8d791e31" data-ci="427972110" data-empn="6902492918904880" data-hiring-event="false" data-hide-spinner="true" role="button" aria-label="full details of Software Engineer" class="jcs-JobTitle css-jspxzf eu4oa1w0" href="https://www.indeed.com/pagead/clk?mo=r&amp;ad=-6NYlbfkN0AuOYgYKKgFz-UHFdX2QsanEhtfJOyi5uX9CjtgXnV2WXXZt5e3yxkDJUlvspRLCM04HzPh47REpZvK17jx5QhV92EbanELcTx3R7zd-VQyrXypaFQ4LyqCwdV71DmBQgDYNiUEP9aPF-gO1oUiSliYKY7bBjMQEGNDMGzi6dHjKV_Mk6yXTNr865xr-vKXn6EfHM_wWNCdALRI4GbUtgNkOVJupRJctHQlzSWOSsyLUuGbp5MA32fFPCPSL_hVw6i-doY8V3Hri6ss0Hhuxh5IACnstI4--onYxciCazUUVgD-vvMKue5Dt2c0DrHzKiwb5RladXwqmwhipjJw6aaY4lNvP1z3uJsMEkC0FTNMGyE_5X8crKCYuLQ4NMuaEna21nBIgCezAc-sAdKKMgYO4-7qBEknQzJoRvxEB75MXOg_xly6BiMwBjNL1tS0oRtCX5MlRGokaV8d2lBuuRUbddkib6T4AD9Ie72jSh2xHLEhaM9ZmltShCHi1UpfG9Qn6tOhmBJbDlZBYzYvY5zW4NVLHSL2sDaC535iVaf4q5EYqz56PLYsTH3uF00PFeUdQacemR13VuyftD7DKkSScFyr2kMYqYviSA3C2hFMEg==&amp;xkcb=SoDN6_M3C08nh-wxhh0KbzkdCdPP&amp;camk=4HOcmqOLYrDd1DYCHSND-g==&amp;p=1&amp;fvj=1&amp;vjs=3&amp;tk=1hrkqa2n3ia30863&amp;jsa=1377&amp;oc=1&amp;sal=1"><span title="Software Engineer" id="jobTitle-414ec77a8d791e31">Software Engineer</span></a>
