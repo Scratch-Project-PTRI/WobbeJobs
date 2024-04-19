5a45298a4b3811b68a1f109c0852f3931c333c49
Tommy Martinez <tommy@Tommys-MacBook-Pro.local>
2024-04-18 21:12:55 -0400

    update to salary

--- server/controllers/indeedController.js
+++ server/controllers/indeedController.js
@@ -30,12 +30,20 @@ indeedController.searchIndeed = async (req, res, next) => {
       console.log(jobTitle);
 
       const priceTitleElement = jobElement.querySelector(
-        '.css-1cvo3fd.eu4oa1w0'
+        '.metadata.salary-snippet-container'
       );
-      const priceTitle = priceTitleElement
-        ? priceTitleElement.textContent.trim()
-        : 'Salary not found';
-      // https://www.indeed.com/rc
+      let priceTitle;
+
+      if (priceTitleElement !== null) {
+        const childPriceElement = priceTitleElement.querySelector(
+          '.css-1cvo3fd.eu4oa1w0'
+        );
+
+        priceTitle = childPriceElement.textContent.trim();
+      } else {
+        priceTitle = 'Salary not found';
+      }
+
       const quickApplyLinkElement = jobElement.querySelector(
         'a.jcs-JobTitle.css-jspxzf.eu4oa1w0'
       );
@@ -70,6 +78,14 @@ indeedController.searchIndeed = async (req, res, next) => {
 };
 
 module.exports = indeedController;
+
+{
+  /* <div class="metadata salary-snippet-container css-5zy3wz eu4oa1w0"><div data-testid="attribute_snippet_testid" class="css-1cvo3fd eu4oa1w0">$75,000 - $78,000 a year</div></div> */
+}
+
+// const priceTitleElement = jobElement.querySelector(
+//   '.css-1cvo3fd.eu4oa1w0'
+// );
 
 //here is the element to scrape
 //  <a id="sj_414ec77a8d791e31" data-mobtk="1hrkqa2n3ia30863" data-jk="414ec77a8d791e31" data-ci="427972110" data-empn="6902492918904880" data-hiring-event="false" data-hide-spinner="true" role="button" aria-label="full details of Software Engineer" class="jcs-JobTitle css-jspxzf eu4oa1w0" href="https://www.indeed.com/pagead/clk?mo=r&amp;ad=-6NYlbfkN0AuOYgYKKgFz-UHFdX2QsanEhtfJOyi5uX9CjtgXnV2WXXZt5e3yxkDJUlvspRLCM04HzPh47REpZvK17jx5QhV92EbanELcTx3R7zd-VQyrXypaFQ4LyqCwdV71DmBQgDYNiUEP9aPF-gO1oUiSliYKY7bBjMQEGNDMGzi6dHjKV_Mk6yXTNr865xr-vKXn6EfHM_wWNCdALRI4GbUtgNkOVJupRJctHQlzSWOSsyLUuGbp5MA32fFPCPSL_hVw6i-doY8V3Hri6ss0Hhuxh5IACnstI4--onYxciCazUUVgD-vvMKue5Dt2c0DrHzKiwb5RladXwqmwhipjJw6aaY4lNvP1z3uJsMEkC0FTNMGyE_5X8crKCYuLQ4NMuaEna21nBIgCezAc-sAdKKMgYO4-7qBEknQzJoRvxEB75MXOg_xly6BiMwBjNL1tS0oRtCX5MlRGokaV8d2lBuuRUbddkib6T4AD9Ie72jSh2xHLEhaM9ZmltShCHi1UpfG9Qn6tOhmBJbDlZBYzYvY5zW4NVLHSL2sDaC535iVaf4q5EYqz56PLYsTH3uF00PFeUdQacemR13VuyftD7DKkSScFyr2kMYqYviSA3C2hFMEg==&amp;xkcb=SoDN6_M3C08nh-wxhh0KbzkdCdPP&amp;camk=4HOcmqOLYrDd1DYCHSND-g==&amp;p=1&amp;fvj=1&amp;vjs=3&amp;tk=1hrkqa2n3ia30863&amp;jsa=1377&amp;oc=1&amp;sal=1"><span title="Software Engineer" id="jobTitle-414ec77a8d791e31">Software Engineer</span></a>
